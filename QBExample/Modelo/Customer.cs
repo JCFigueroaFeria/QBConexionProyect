4f20f2ae7a455484c36cb53285466567b83b70c6
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-12 10:14:38 -0500

    se agrego los atributos para obener los datos de los customer

---
+++ QBExample/Modelo/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Customer
+    {
+        private string CompanyName;
+        private string JobName;
+        private string BillTo;
+        public Customer() {
+
+            CompanyName = "";
+            JobName = "";
+            BillTo = "";
+        }
+
+        public string COMPANYNAME { get { return CompanyName; } set { CompanyName = value; } }
+        public string JOBNAME { get { return JobName; } set { JobName = value; } }
+        public string BILLTO { get { return BillTo; } set { BillTo = value; } }
+    }
+}
